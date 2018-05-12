11681e763650952b577132d82539cceb81642b9a
Marko Knöbl <marko.kn@gmail.com>
2018-05-12 19:51:16 +0200

    Einheit 13

---
+++ einheit13/postreq/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import os
+import jinja2
+import webapp2
+
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if params is None:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        return self.render_template("hello.html")
+
+class ShoutHandler(BaseHandler):
+    def post(self):
+        # lies den request-parameter 'message' aus
+        # und speichere ihn unter der Python-Variable 'message'
+        message = self.request.get("message")
+        return self.render_template(
+            "shout_result.html",
+            params = {"message": message}
+        )
+        #return self.write('<h1>' + message.upper() + '</h1>')
+
+class CalcHandler(BaseHandler):
+    def post(self):
+        number1 = float(self.request.get("number1"))
+        number2 = float(self.request.get("number2"))
+        sum = number1 + number2
+        return self.render_template(
+            "calc_result.html",
+            params = {"number1": number1, "number2": number2, "sum": sum}
+        )
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+    webapp2.Route('/shout', ShoutHandler),
+    webapp2.Route('/calc', CalcHandler),
+], debug=True)
